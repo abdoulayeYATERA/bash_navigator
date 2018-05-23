5ed014933238e76db535b19b9f331d99fdd978fd
abdoulaye yatera <yatera.a@outlook.com>
2018-05-23 23:57:35 +0200

    update license

--- bash_navigator.sh
+++ bash_navigator.sh
@@ -1,23 +1,6 @@
-#Copyright (c) 2018 Abdoulaye Yatera
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-#
+# Copyright MIT Licence (c) 2018 Abdoulaye Yatera
+# bash_navigator is a tool to jump quickly in your bash navigation history. 
+# read the README.md file for more details
 # project on github : https://github.com/abdoulayeYATERA/bash_navigator
 
 # it is important to notice that each shell instance will have
